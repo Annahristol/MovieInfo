9d3c409569cd714b8900da0e689ace94eb769502
Annahristol <anna.hristol@mail.ru>
2017-12-21 12:25:06 +0300

    Добавьте файлы проекта.

---
+++ MovieInfo/Movie.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MovieInfo
+{
+    class MovieApi
+    {
+        public Movie GetMovieInfo(string query)
+        {
+            string result = null;
+            query = WebUtility.UrlEncode(query);
+            try
+            {
+                WebRequest request = WebRequest.Create($"http://www.omdbapi.com/?apikey=80c2e706&t={query}&plot=full");
+                WebResponse response = request.GetResponse();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    result = reader.ReadToEnd();
+            }
+            catch
+            {
+                MessageBox.Show("Connection error");
+                return null;
+            }
+            if (result == null) return null;
+            Movie movie = null;
+            try
+            {
+                movie = JsonConvert.DeserializeObject<Movie>(result);
+            }
+            catch
+            {
+                MessageBox.Show("Can't load result");
+                return null;
+            }
+            return movie;
+        }
+    }
+    class Movie
+    {
+        public string Response;
+        public string Error;
+        public string Title;
+        public string Year;
+        public string Rated;
+        public string Runtime;
+        public string Genre;
+        public string Plot;
+        public string Type;
+        public string Poster;
+        public string imdbID;
+    }
+}
---
+++ MovieInfo/ViewModel.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MovieInfo
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        private ImageSource _poster;
+        public ImageSource Poster
+        {
+            get { return _poster; }
+            set
+            {
+                _poster = value;
+                OnPropertyChanged("Poster");
+            }
+        }
+        private string _searchBox;
+        public string SearchBox
+        {
+            get { return _searchBox; }
+            set
+            {
+                _searchBox = value;
+                OnPropertyChanged("SearchBox");
+            }
+        }
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        private string _year { get; set; }
+        public string Year
+        {
+            get { return _year; }
+            set
+            {
+                _year = value;
+                OnPropertyChanged("Year");
+            }
+        }
+        private string _rating { get; set; }
+        public string Rating
+        {
+            get { return _rating; }
+            set
+            {
+                _rating = value;
+                OnPropertyChanged("Rating");
+            }
+        }
+        private string _runtime { get; set; }
+        public string Runtime
+        {
+            get { return _runtime; }
+            set
+            {
+                _runtime = value;
+                OnPropertyChanged("Runtime");
+            }
+        }
+        private string _genre { get; set; }
+        public string Genre
+        {
+            get { return _genre; }
+            set
+            {
+                _genre = value;
+                OnPropertyChanged("Genre");
+            }
+        }
+        private string _plot;
+        public string Plot
+        {
+            get { return _plot; }
+            set
+            {
+                _plot = value;
+                OnPropertyChanged("Plot");
+            }
+        }
+        private bool _isFave;
+        public bool IsFave
+        {
+            get { return _isFave; }
+            set
+            {
+                _isFave = value;
+                OnPropertyChanged("IsFave");
+            }
+        }
+        public Command Search { get; set; } = new Command();
+        public Command Fave { get; set; } = new Command();
+        public ViewModel()
+        {
+            Search.Method = GoSearch;
+            Fave.Method = GoSave;
+        }
+
+        private async void GoSave(object obj)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=Movies;Integrated Security=True"))
+            {
+                SqlCommand request = new SqlCommand($"insert into Faves(imdbID) values (\'{movie.imdbID}\')", connection);
+                try
+                {
+                    await connection.OpenAsync();
+                    await request.ExecuteNonQueryAsync();
+                    connection.Close();
+                    IsFave = false;
+                }
+                catch
+                {
+                    MessageBox.Show("SQL bad response");
+                }
+            }
+        }
+        private Movie movie;
+        private void GoSearch(object obj)
+        {
+            IsFave = false;
+            Poster = new BitmapImage();
+            Title = string.Empty;
+            Year = string.Empty;
+            Rating = string.Empty;
+            Runtime = string.Empty;
+            Genre = string.Empty;
+            Plot = string.Empty;
+            MovieApi api = new MovieApi();
+            movie = api.GetMovieInfo(SearchBox);
+            if (movie == null) return;
+            if (movie.Response == "False")
+            {
+                MessageBox.Show(movie.Error);
+                return;
+            }
+            Title = movie.Type == "movie" ? movie.Title : $"({movie.Type}) {movie.Title}";
+            Year = movie.Year;
+            Rating = movie.Rated;
+            Runtime = movie.Runtime;
+            Genre = movie.Genre.Replace(", ", ",\n");
+            Plot = movie.Plot;
+            try
+            {
+                using (WebClient c = new WebClient())
+                    c.DownloadFile(movie.Poster, "poster.png");
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.CacheOption = BitmapCacheOption.OnLoad;
+                img.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                img.UriSource = new Uri(AppDomain.CurrentDomain.BaseDirectory + "poster.png", UriKind.Absolute);
+                img.EndInit();
+                Poster = img;
+            }
+            catch
+            {
+                MessageBox.Show("Can't load poster");
+            }
+            IsFave = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+    class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public Action<object> Method { get; set; }
+        public void Execute(object parameter)
+        {
+            Method(parameter);
+        }
+    }
+}
